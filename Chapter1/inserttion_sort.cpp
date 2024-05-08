5ddbb060ca01b20882358548b0b09895edeb1e70
Coji <110469813+bubbleShaker@users.noreply.github.com>
2024-05-08 22:28:19 +0900

    Update inserttion_sort.cpp

--- Chapter1/inserttion_sort.cpp
+++ Chapter1/inserttion_sort.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> insertion_sort(vector<int>& A){
+vector<int> INSERTION_SORT(vector<int>& A){
     int n=A.size();
     for(int j=1;j<n;j++){
         int key=A[j];
@@ -15,7 +15,7 @@ vector<int> insertion_sort(vector<int>& A){
 
 int main() {
     vector<int> a={5,2,4,6,1,3};
-    a=insertion_sort(a);
+    a=INSERTION_SORT(a);
     int n=a.size();
     for(int i=0;i<n;i++){
         cout<<a[i]<<" \n"[i+1==n];
