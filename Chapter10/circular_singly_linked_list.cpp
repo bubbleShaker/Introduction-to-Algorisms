c8843fc6bf1708c2820a9580d50558a5b6a341cd
Coji <110469813+bubbleShaker@users.noreply.github.com>
2024-05-09 09:55:01 +0900

    Chapter10

---
+++ Chapter10/circular_singly_linked_list.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T>
+struct Node{
+    T key;
+    Node<T>* next;
+};
+template<typename T>
+class CircularSinglyLinkedList{
+private:
+    Node<T>* head;
+    Node<T>* tail;
+public:
+    CircularSinglyLinkedList():head(nullptr),tail(nullptr) {}
+    void LIST_INSERT(Node<T>* x){
+        if(head==nullptr){
+            head=x;
+        }
+        if(tail!=nullptr){
+            tail->next=x;
+        }
+        tail=x;
+        tail->next=head;
+    }
+    void LIST_DELETE(Node<T>* x){
+        Node<T>* ptr=head;
+        Node<T>* prevPtr=nullptr;
+        while(ptr!=nullptr&&ptr!=x){
+            prevPtr=ptr;
+            ptr=ptr->next;
+        }
+        if(ptr==x&&prevPtr==nullptr){
+            head=ptr->next;
+        }else if(ptr==x){
+            prevPtr->next=ptr->next;
+        }
+    }
+    Node<T>* LIST_SEARCH(T k){
+        Node<T>* ptr=head;
+        while(ptr!=tail&&ptr->key!=k){
+            ptr=ptr->next;
+        }
+        if(ptr->key!=k){
+            return nullptr;
+        }else{
+            return ptr;
+        }
+    }
+    void print(){
+        Node<T>* ptr=head;
+        int cnt=0;
+        do{
+            cout<<ptr->key<<((ptr->next!=head)?"->":"\n");
+            ptr=ptr->next;
+        }while(ptr!=head);
+    }
+};
+int main() {
+    CircularSinglyLinkedList<int> cslist;
+    Node<int>* node1=new Node<int>(4,nullptr);
+    Node<int>* node2=new Node<int>(1,nullptr);
+    Node<int>* node3=new Node<int>(2,nullptr);
+    cslist.LIST_INSERT(node1);
+    cslist.LIST_INSERT(node2);
+    cslist.LIST_INSERT(node3);
+    cslist.print();
+    cslist.LIST_DELETE(node2);
+    cslist.print();
+    cout<<((cslist.LIST_SEARCH(4)!=nullptr)?to_string(cslist.LIST_SEARCH(4)->key):"nullptr")<<endl;
+    cout<<((cslist.LIST_SEARCH(7)!=nullptr)?to_string(cslist.LIST_SEARCH(7)->key):"nullptr")<<endl;
+    return 0;
+}
